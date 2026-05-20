51ce26c266f70734641943956309f3732431b8d8
faizan4dev <faizan4dev@gmail.com>
2026-05-20 12:55:57 +0500

    Messaging with bot started working fine

--- js/api.js
+++ js/api.js
@@ -1,15 +1,19 @@
 async function getGeminiResponse(prompt) {
   let url =
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyCGahNncg9c8VIVSbhdO_7PAv0Wx0KWtSQ";
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyAcdcAazQYCq3CE6pOZ4-234OAV6DWrP2s";
 
-  let resp = await axios.post(url, {
-    contents: [
-      {
-        parts: [{ text: prompt }],
-      },
-    ],
-  });
+  try {
+    let resp = await axios.post(url, {
+      contents: [
+        {
+          parts: [{ text: prompt }],
+        },
+      ],
+    });
 
-  console.log(resp.data.candidates[0].content.parts[0].text);
-  //   console.log(resp);
+    return resp.data.candidates[0].content.parts[0].text;
+    //   console.log(resp);
+  } catch (error) {
+    return "Something went Wrong";
+  }
 }
